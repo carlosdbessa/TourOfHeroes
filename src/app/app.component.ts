c2e0078038d5e99f418a7cdba6a5f85c05b68f56
Utilizador78 <carlbessa1618@gmail.com>
2024-01-30 15:38:55 +0000

    error HeroesComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from "./heroes/heroes.component";
 
+
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     imports: [RouterOutlet, HeroesComponent]
+
+    
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
 }
